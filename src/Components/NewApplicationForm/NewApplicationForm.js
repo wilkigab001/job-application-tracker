0a0d77ef1e29baacc01e17f0ba48cd22a68e2e66
Gabriel Wilkins <wilkigab001@gmail.com>
2022-12-19 12:54:25 -0700

    This has all updated new application page

--- src/Components/NewApplicationForm/NewApplicationForm.js
+++ src/Components/NewApplicationForm/NewApplicationForm.js
@@ -1,57 +1,68 @@
-import React, {useState, useContext} from 'react'
-import styled from 'styled-components'
-import axios from 'axios'
-import AuthContext from '../../store/authContext'
+import React, { useState, useContext } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import AuthContext from "../../store/authContext";
 const NewApplicationForm = () => {
+  const { token, userId } = useContext(AuthContext);
 
-  const {token, userId} = useContext(AuthContext)
+  const [jobTitle, setJobTitle] = useState("");
+  const [applicationDate, setApplicationDate] = useState("");
+  const [hiringManager, setHiringManager] = useState("");
+  const [interviewStatus, setInterviewStatus] = useState(false);
+  const [jobPostingLink, setJobPostingLink] = useState("");
 
-  const [jobTitle, setJobTitle] = useState('')
-  const [applicationDate, setApplicationDate] = useState('')
-  const [hiringManager, setHiringManager] = useState('')
-  const [interviewStatus, setInterviewStatus] = useState('')
-  const [jobPostingLink, setJobPostingLink] = useState('')
-
-  const submitHandler = () => {
-
-  }
+  const submitHandler = () => {};
   return (
     <Wrapper>
-      <Form>
-        <JobTitleInput placeholder='Job Title'/>
-        <ApplicationDate placeholder='When did you apply?'/>
-        <HiringManagerInput placeholder='Hiring Manager' />
+      <Form onSubmit={submitHandler}>
+        <JobTitleInput
+          placeholder="Job Title"
+          value={jobTitle}
+          onChange={(e) => setJobTitle(e.target.value)}
+        />
+        <ApplicationDate
+          placeholder="When did you apply?"
+          value={applicationDate}
+          onChange={(e) => setApplicationDate(e.target.value)}
+        />
+        <HiringManagerInput
+          placeholder="Hiring Manager"
+          value={hiringManager}
+          onChange={(e) => setHiringManager(e.target.value)}
+        />
         <h3>Heard Back Yet?</h3>
         <Item>
-          <RadioButton 
-          type='radio'
-          name='radio'
-          value='true'
+          <RadioButton
+            type="radio"
+            name="radio"
+            value={true}
+            onChange={(e) => setInterviewStatus(e.target.value)}
           />
           <RadioButtonLabel>Yes</RadioButtonLabel>
         </Item>
         <Item>
-        <RadioButton 
-          type='radio'
-          name='radio'
-          value='false'
+          <RadioButton
+            type="radio"
+            name="radio"
+            value={false}
+            onChange={(e) => setInterviewStatus(e.target.value)}
           />
           <RadioButtonLabel>No</RadioButtonLabel>
         </Item>
-        <SubmitButton>Submit </SubmitButton>
+        <SubmitButton> Submit </SubmitButton>
       </Form>
     </Wrapper>
-  )
-}
+  );
+};
 
-const Wrapper = styled.div``
-const Form = styled.form``
-const JobTitleInput = styled.input``
-const HiringManagerInput = styled.input``
-const ApplicationDate = styled.input``
-const jobPostingLinkInput = styled.input``
-const SubmitButton = styled.button``
-const Item = styled.div``
-const RadioButton = styled.input``
-const RadioButtonLabel = styled.label``
-export default NewApplicationForm
+const Wrapper = styled.div``;
+const Form = styled.form``;
+const JobTitleInput = styled.input``;
+const HiringManagerInput = styled.input``;
+const ApplicationDate = styled.input``;
+const jobPostingLinkInput = styled.input``;
+const SubmitButton = styled.button``;
+const Item = styled.div``;
+const RadioButton = styled.input``;
+const RadioButtonLabel = styled.label``;
+export default NewApplicationForm;
